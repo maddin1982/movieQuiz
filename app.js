e94676bfe4303b06a2753b02315b29d5975b75c7
martin <martin.herrmann@visualistik.de>
2019-12-05 23:27:38 +0100

    upload 3 files and add to jjson

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const FileSync = require('lowdb/adapters/FileSync');
 
 const adapter = new FileSync('db.json');
 const db = low(adapter);
-db.defaults({ images: [] }).write();
+//db.defaults({ movies: [] }).write();
 
 app.use(fileUpload());
 app.use(express.static('public'));
@@ -17,27 +17,31 @@ app.post('/upload', function(req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
+  let lastIndex = db.get('lastIndex')|| 0;
+  lastIndex++;
+  let movies = [];
 
-  const image = {
-    id: 1,
-    name: 'new Movie',
-    linkedTo: '2'
-  };
+  for(let file in req.files) {
+    let filmName = req.body[file + '_name'];
 
-  db.get('images').push(image).write();
+    movies.push(filmName);
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+    req.files[file].mv(__dirname + '/images/' + lastIndex + '_' +filmName + '.jpg', function(err) {
+      if (err)
+        return res.status(500).send(err);
 
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(__dirname + '/images/filename.jpg', function(err) {
-    if (err)
-      return res.status(500).send(err);
+    });
+  }
 
-    res.send('File uploaded!');
-  });
+
+// Increment count
+
+  db.get('movies').push({'id':lastIndex, 'movies' : movies}).write();
+  db.set('lastIndex', lastIndex).write();
+
+  res.redirect('/');
 });
 
 
 console.log('app listening on port', port);
-app.listen(port)
+app.listen(port);
